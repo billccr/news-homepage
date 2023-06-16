ee7b0394072dc981783721d25cb58aceeec669cc
Bill Cabrera <bill@chubbycrowrecords.com>
2023-06-15 22:10:48 -0400

    set up slide-in mobile menu with opacity overlay

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Set the width of the side navigation to 250px and the
+// left margin of the page content to 250px and add a black background color to body
+function openNav() {
+  document.getElementById("navmenu").style.width = "250px";
+//  document.getElementById("main").style.marginRight = "250px";
+  document.querySelector(".container").style.backgroundColor = "rgba(0,0,0,0.6)";
+}
+
+// Set the width of the side navigation to 0 and the
+// left margin of the page content to 0, and the background color of body to white
+function closeNav() {
+  document.getElementById("navmenu").style.width = "0";
+//  document.getElementById("main").style.marginRight = "0";
+  document.body.style.backgroundColor = "white";
+}
